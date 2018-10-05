7d16a2f9de8019c78ee9cff7b14472d666211310
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-05 16:07:59 -0400

    Add basic socket.io functionality.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const io = require('socket.io')(http);
+
+
+// SOCKET.IO
+
+/*
+// Heroku compatability.
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});*/
+
+io.on('connection', (socket)=>{
+    console.log("Connection event. "+socket.id);
+});
+
+// EXPRESS
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello, World!</h1>');
+
+    // req is http request info.
+    // res is http response.
+
+    res.sendFile(__dirname+'/index.html');
+
+});
+
+app.get('/trivia',(req,res)=>{
+
+    // req is http request info.
+    // res is http response.
+
+    res.send("<h1>Not Yet</h1>");
+
 });
 
 http.listen(port, ()=>{
-    console.log("Listening on "+port)
+    console.log("Success! Listening on "+port)
 });
