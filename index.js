08e76b9f21b547c2d88df5afccc9f428590a2351
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-05 10:39:36 -0400

    Allow Heroku to set port.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello, World!</h1>');
 });
 
-http.listen(3000, ()=>{
-    console.log("Listening on 3000.")
+http.listen(port, ()=>{
+    console.log("Listening on "+port)
 });
