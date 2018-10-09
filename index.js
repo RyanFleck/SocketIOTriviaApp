bc0ae0cbebebaeb0b0ceea227e1f32e69021c31a
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-08 22:33:18 -0400

    OOP: Make user class and message object.

--- index.js
+++ index.js
@@ -5,23 +5,68 @@ const io = require('socket.io')(http);
 
 let users_online = 0;
 
+// Data
+
+class UserBlob {
+    constructor(name){
+    this.name = name;
+    this.color = "#43ca43";
+    this.team = "None";
+    }
+
+    format_message_object( message ){
+        return {
+            name: this.name,
+            color: this.color,
+        }
+    }
+
+    update_color(){
+
+    }
+
+    update_team(){
+
+    }
+
+    send_message( messageout ){
+        io.emit('message out', {
+            username: this.name,
+            usercolor: this.color,
+            message: messageout
+        });
+    }
+
+    announce( messageout ){
+        io.emit('announce', {
+            username: this.name,
+            usercolor: this.color,
+            message: messageout
+        });
+    }
+
+
+}
+
+
 // SOCKET.IO
 
-
 io.on('connection', (socket)=>{
-    let user = socket.id;
+    const user = new UserBlob(socket.id.replace(/\W/g, '').slice(0,5).toUpperCase());
     users_online += 1;
-    io.emit('message out', "User "+user+" has joined. Users online: "+users_online);
+    user.announce("has joined the chat!");
+    //io.emit('message out', "User "+user.name+" has joined. Users online: "+users_online);
 
-    console.log("Connection event. User "+user);
-    socket.on('disconnect',(user)=>{
+    console.log("Connection event. User "+user.name);
+    socket.on('disconnect',()=>{
         users_online -= 1;
-        console.log("User "+user+" disconnected.");
+        user.announce("has disconnected.");
+        console.log("User "+user.name+" disconnected.");
     });
 
     socket.on('message', (message)=>{
-        console.log(user+" sent message: "+message);
-        io.emit('message out', message);
+        console.log(user.name+" sent message: "+message);
+        user.send_message(message);
     });
 });
 
