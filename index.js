56a01d875f2683c0bfeddb0211f33fa98659e83a
Ryan Fleck <ryan.fleck@protonmail.com>
2018-10-09 16:05:22 -0400

    Fix linted problems.

--- index.js
+++ index.js
@@ -1,95 +1,91 @@
 const app = require('express')();
 const http = require('http').Server(app);
+
 const port = process.env.PORT || 3000;
 const io = require('socket.io')(http);
 
-let users_online = 0;
+let usersOnline = 0;
 
 // Data
 
 class UserBlob {
-    constructor(name){
-    this.name = name;
-    this.color = "#43ca43";
-    this.team = "None";
+    constructor(name) {
+        this.name = name;
+        this.color = '#43ca43';
+        this.team = 'None';
     }
 
-    format_message_object( message ){
+    formatMessageObject(fmessage) {
         return {
             name: this.name,
             color: this.color,
-        }
+            message: fmessage,
+        };
     }
 
-    update_color(){
-
+    updateColor() {
+        return this.color;
     }
 
-    update_team(){
-
+    updateTeam() {
+        return this.team;
     }
 
-    send_message( messageout ){
+    sendMessage(messageout) {
         io.emit('message out', {
             username: this.name,
             usercolor: this.color,
-            message: messageout
+            message: messageout,
         });
     }
 
-    announce( messageout ){
+    announce(messageout) {
         io.emit('announce', {
             username: this.name,
             usercolor: this.color,
-            message: messageout
+            message: messageout,
         });
     }
-
-
 }
 
 
 // SOCKET.IO
 
-io.on('connection', (socket)=>{
-    const user = new UserBlob(socket.id.replace(/\W/g, '').slice(0,5).toUpperCase());
-    users_online += 1;
-    user.announce("has joined the chat!");
-    //io.emit('message out', "User "+user.name+" has joined. Users online: "+users_online);
+io.on('connection', (socket) => {
+    const user = new UserBlob(socket.id.replace(/\W/g, '').slice(0, 5).toUpperCase());
+    usersOnline += 1;
+    user.announce('has joined the chat!');
+    // io.emit('message out', "User "+user.name+" has joined. Users online: "+usersOnline);
 
-    console.log("Connection event. User "+user.name);
-    socket.on('disconnect',()=>{
-        users_online -= 1;
-        user.announce("has disconnected.");
-        console.log("User "+user.name+" disconnected.");
+    console.log(`Connection event. User ${  user.name}`);
+    socket.on('disconnect', () => {
+        usersOnline -= 1;
+        user.announce('has disconnected.');
+        console.log(`User ${  user.name  } disconnected.`);
     });
 
-    socket.on('message', (message)=>{
-        console.log(user.name+" sent message: "+message);
-        user.send_message(message);
+    socket.on('message', (message) => {
+        console.log(`${user.name} sent message: ${message}`);
+        user.sendMessage(message);
     });
 });
 
 // EXPRESS
 
-app.get('/',(req,res)=>{
-
+app.get('/', (req, res) => {
     // req is http request info.
     // res is http response.
 
-    res.sendFile(__dirname+'/index.html');
-
+    res.sendFile(`${__dirname}/index.html`);
 });
 
-app.get('/trivia',(req,res)=>{
-
+app.get('/trivia', (req, res) => {
     // req is http request info.
     // res is http response.
 
-    res.send("<h1>Not Yet</h1>");
-
+    res.send('<h1>Not Yet</h1>');
 });
 
-http.listen(port, ()=>{
-    console.log("Success! Listening on "+port)
+http.listen(port, () => {
+    console.log(`Success! Listening on ${port}. ${usersOnline} users online.`);
 });
